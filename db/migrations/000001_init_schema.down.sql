07395134dd3bf46f3672693c8b30d4eb192a95f1
nehal@gather.network <nehal.shaquib21@gmail.com>
2023-07-22 21:10:46 +0530

    added db migrations

---
+++ db/migrations/000001_init_schema.down.sql
@@ -0,0 +1,3 @@
+DROP TABLE attestation_details;
+DROP TABLE attestation_duties;
+DROP TABLE block_headers;
---
+++ db/migrations/000001_init_schema.up.sql
@@ -0,0 +1,62 @@
+CREATE TABLE "block_headers" (
+  "slot" bigInt PRIMARY KEY NOT NULL,
+  "root" varchar NOT NULL,
+  "canonical" bool NOT NULL,
+  "proposer_index" bigInt NOT NULL,
+  "parent_root" varchar NOT NULL,
+  "state_root" varchar NOT NULL,
+  "body_root" varchar NOT NULL,
+  "signature" varchar NOT NULL,
+  "finalized" bool NOT NULL,
+  "execution_optimistic" bool NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "attestation_duties" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "dependent_root" varchar NOT NULL,
+  "execution_optimistic" bool NOT NULL,
+  "pubkey" varchar NOT NULL,
+  "validator_index" bigInt NOT NULL,
+  "committees_at_slot" bigInt NOT NULL,
+  "committee_index" bigInt NOT NULL,
+  "committee_length" bigInt NOT NULL,
+  "validator_committee_index" bigInt NOT NULL,
+  "slot" bigInt NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "attestation_details" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "execution_optimistic" bool NOT NULL,
+  "finalized" bool NOT NULL,
+  "aggregation_bits" varchar NOT NULL,
+  "slot" bigInt NOT NULL,
+  "index" bigInt NOT NULL,
+  "beacon_block_root" varchar NOT NULL,
+  "source_epoch" bigInt NOT NULL,
+  "source_root" varchar NOT NULL,
+  "target_epoch" bigInt NOT NULL,
+  "target_root" varchar NOT NULL,
+  "signature" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "block_headers" ("root");
+
+CREATE INDEX ON "block_headers" ("proposer_index");
+
+CREATE INDEX ON "attestation_duties" ("pubkey");
+
+CREATE INDEX ON "attestation_duties" ("validator_index");
+
+CREATE INDEX ON "attestation_duties" ("committee_index");
+
+CREATE INDEX ON "attestation_duties" ("slot");
+
+CREATE INDEX ON "attestation_details" ("slot");
+
+ALTER TABLE "attestation_duties" ADD FOREIGN KEY ("slot") REFERENCES "block_headers" ("slot");
+
+ALTER TABLE "attestation_details" ADD FOREIGN KEY ("slot") REFERENCES "block_headers" ("slot");
+
