3f30b99f32ca70bb21f14c6e30d7c292b108295c
nehal@gather.network <nehal.shaquib21@gmail.com>
2023-07-23 01:09:38 +0530

    implemented block header indexer

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/nehalshaquib/ethereum-beacon-chain-indexer.git/config"
+	"github.com/nehalshaquib/ethereum-beacon-chain-indexer.git/db"
+	"github.com/nehalshaquib/ethereum-beacon-chain-indexer.git/indexer"
+)
+
+func main() {
+	err := config.Config()
+	if err != nil {
+		log.Println("ERR: config - ", err)
+	}
+
+	db := db.NewDatabase()
+
+	indexer := indexer.New(db)
+	indexer.IndexHeader()
+
+}
